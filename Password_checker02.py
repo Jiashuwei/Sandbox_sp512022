ec1a22ca10f4925c50821c370099bc363fef91c7
Jiashuwei <87465038+Jiashuwei@users.noreply.github.com>
2022-03-30 21:38:01 +0800

    Add files via upload

---
+++ Password_checker02.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Practical
+
+Description: A password checking
+
+Name: Jiashu Wei
+"""
+
+def main():
+    input_pass = get_password()
+    print_asterisk(input_pass)
+
+def print_asterisk(input_pass):
+    print(len(input_pass)*'*')
+
+def get_password():
+    input_pass = input('Type Password: ')
+    while len(input_pass) <= 1:
+        input_pass = input('Type Password: ')
+    return input_pass
